3b99bbe0f16034484040c26c8d55995a075a5237
19853 <myfamilyis666>
2022-08-24 19:18:19 +0800

    第五阶段5

--- csmall-business/src/main/java/cn/tedu/csmall/business/controller/BusinessController.java
+++ csmall-business/src/main/java/cn/tedu/csmall/business/controller/BusinessController.java
@@ -1,7 +1,11 @@
 package cn.tedu.csmall.business.controller;
 
 import cn.tedu.csmall.business.service.IBusinessService;
+import cn.tedu.csmall.commons.pojo.stock.dto.StockReduceCountDTO;
 import cn.tedu.csmall.commons.restful.JsonResult;
+import cn.tedu.csmall.commons.restful.ResponseCode;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,10 +24,20 @@ public class BusinessController {
     @PostMapping("/buy")
     // localhost:20000/base/business/buy(必须是post请求,浏览器输入地址无效)
     @ApiOperation("执行触发的方法")
+    @SentinelResource(value = "购买业务",blockHandler = "blockError",fallback = "fallbackError")
     public JsonResult buy(){
         // 调用业务逻辑层的方法
         businessService.buy();
         return JsonResult.ok("购买完成!");
+    }
+
+    public JsonResult blockError(BlockException e){
+        return JsonResult.failed(ResponseCode.INTERNAL_SERVER_ERROR,"服务器忙，请稍后再试");
+    }
+
+    public JsonResult fallbackError(BlockException e){
+        return JsonResult.failed(ResponseCode.INTERNAL_SERVER_ERROR,"运行发生异常，服务降级");
+
     }
 
 }
--- csmall-stock/csmall-stock-webapi/src/main/java/cn/tedu/csmall/stock/webapi/controller/StockController.java
+++ csmall-stock/csmall-stock-webapi/src/main/java/cn/tedu/csmall/stock/webapi/controller/StockController.java
@@ -1,9 +1,12 @@
 package cn.tedu.csmall.stock.webapi.controller;
 
+import cn.tedu.csmall.commons.exception.CoolSharkServiceException;
 import cn.tedu.csmall.commons.pojo.stock.dto.StockReduceCountDTO;
 import cn.tedu.csmall.commons.restful.JsonResult;
+import cn.tedu.csmall.commons.restful.ResponseCode;
 import cn.tedu.csmall.stock.service.IStockService;
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,10 +23,22 @@ public class StockController {
 
     @PostMapping("/reduce/count")
     @ApiOperation("减少商品库存数量")
-    @SentinelResource("减少库存的方法")
+    @SentinelResource(value = "减少库存的方法",blockHandler = "blockError",fallback = "fallbackError")
     public JsonResult reduceCommodityCount(StockReduceCountDTO stockReduceCountDTO){
+
+        if (Math.random()<0.5){
+            throw new CoolSharkServiceException(ResponseCode.BAD_REQUEST,"随机异常");
+        }
+
         stockService.reduceCommodityCount(stockReduceCountDTO);
         return JsonResult.ok("库存减少已执行");
     }
+
+    public JsonResult blockError(StockReduceCountDTO stockReduceCountDTO, BlockException e){
+        return JsonResult.failed(ResponseCode.INTERNAL_SERVER_ERROR,"服务器忙,请稍后再试");
+    }
+
+    public JsonResult fallbackError(StockReduceCountDTO stockReduceCountDTO){
+        return JsonResult.failed(ResponseCode.INTERNAL_SERVER_ERROR,"运行发生异常，服务降级");
+    }
 }
-  
